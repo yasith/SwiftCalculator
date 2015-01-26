9fdbf90b15312152d35bfe8efc216879168a1287
Yasith Vidanaarachchi <yasith@tuxv.net>
2015-01-25 21:44:36 -0500

    Calculator working with +-*/ functions.

--- SwiftCalculator/ViewController.swift
+++ SwiftCalculator/ViewController.swift
@@ -29,8 +29,34 @@ class ViewController: UIViewController {
     @IBAction func enter() {
         operandStack.append(displayValue)
         isUserTyping = false
+        display.text = "0"
         
         println("\(operandStack)")
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if isUserTyping {
+            enter()
+        }
+        
+        switch operation {
+        
+        case "+": performOperation {$0 + $1}
+        case "-": performOperation {$1 - $0}
+        case "×": performOperation {$0 * $1}
+        case "÷": performOperation {$1 / $0}
+            
+        default:
+            break
+        }
+    }
+    
+    func performOperation(operation : (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            let newVal = operation(operandStack.removeLast(), operandStack.removeLast())
+            display.text = "\(newVal)"
+        }
     }
     
     var displayValue : Double {
